f6007f3e9a2be7ed2159cd4bd5ebf128f1355d0c
afnanaltaweel <37228227+afnanaltaweel@users.noreply.github.com>
2022-10-03 13:36:58 +0300

    this is the second task

---
+++ src/Second.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Second {
+    public static void main(String[] args) throws InterruptedException {
+
+        // declaration and instantiation of objects/variables
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Afnan Altaweel\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+
+        // Launch website
+        driver.navigate().to("http://www.google.com/");
+
+        // Click on the search text box and send value
+        driver.findElement(By.name("q")).sendKeys("selenium");
+
+        // Click on the search button
+        driver.findElement(By.className("gNO89b")).submit();
+
+        //choose a result
+        driver.findElement(By.partialLinkText("selenium.dev")).click();
+
+
+
+
+
+
+
+    }
+}
+
+
